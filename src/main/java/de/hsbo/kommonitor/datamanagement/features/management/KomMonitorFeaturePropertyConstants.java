7a2560d3f464ecb8d2646ec7f2b71ac084a662cb
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-07-19 08:10:01 +0200

    refactoring. Extract String value as Constant to make it reusable by other components

--- src/main/java/de/hsbo/kommonitor/datamanagement/features/management/KomMonitorFeaturePropertyConstants.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/features/management/KomMonitorFeaturePropertyConstants.java
@@ -5,5 +5,8 @@ public class KomMonitorFeaturePropertyConstants {
 	public static final String VALID_START_DATE_NAME = "validStartDate";
 	public static final String VALID_END_DATE_NAME = "validEndDate";
 	public static final String ARISEN_FROM_NAME = "arisenFrom";
+	
+	
+	public static final String SPATIAL_UNIT_ID_NAME = "spatialUnitId";
 
 }
