62c25694835c9bc49360c733c1d291ea39e0a461
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-07-20 11:43:13 +0200

    add missing class

---
+++ src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/util/DateTimeUtil.java
@@ -0,0 +1,32 @@
+package de.hsbo.kommonitor.datamanagement.api.impl.util;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class DateTimeUtil {
+
+	public static String toISO8601UTC(Date date) {
+		TimeZone tz = TimeZone.getTimeZone("UTC");
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+		df.setTimeZone(tz);
+		return df.format(date);
+	}
+
+	public static Date fromISO8601UTC(String dateStr) {
+		TimeZone tz = TimeZone.getTimeZone("UTC");
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+		df.setTimeZone(tz);
+
+		try {
+			return df.parse(dateStr);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
