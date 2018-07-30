d87f207d300bf24fee8268e1d4a2b40293557659
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-07-30 15:45:06 +0200

    implement ScriptController

--- src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/scripts/ScriptController.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/scripts/ScriptController.java
@@ -1,5 +1,7 @@
 package de.hsbo.kommonitor.datamanagement.api.impl.scripts;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
@@ -14,6 +18,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.hsbo.kommonitor.datamanagement.api.ProcessScriptsApi;
 import de.hsbo.kommonitor.datamanagement.api.impl.BasePathController;
+import de.hsbo.kommonitor.datamanagement.api.impl.exception.ResourceNotFoundException;
+import de.hsbo.kommonitor.datamanagement.api.impl.util.ApiUtils;
+import de.hsbo.kommonitor.datamanagement.model.roles.RoleOverviewType;
 import de.hsbo.kommonitor.datamanagement.model.scripts.ProcessScriptOverviewType;
 import de.hsbo.kommonitor.datamanagement.model.scripts.ProcessScriptPOSTInputType;
 import de.hsbo.kommonitor.datamanagement.model.scripts.ProcessScriptPUTInputType;
@@ -38,14 +45,61 @@ public class ScriptController extends BasePathController implements ProcessScrip
 
 	@Override
 	public ResponseEntity addProcessScriptAsBody(ProcessScriptPOSTInputType processScriptData) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Received request to insert new process script");
+
+		String accept = request.getHeader("Accept");
+
+		/*
+		 * analyse input data and save it within database
+		 */
+		String scriptId;
+		try {
+			scriptId = scriptManager.addScript(processScriptData);
+		} catch (Exception e1) {
+			return ApiUtils.createResponseEntityFromException(e1);
+
+		}
+
+		if (scriptId != null) {
+			HttpHeaders responseHeaders = new HttpHeaders();
+
+			String location = scriptId;
+			try {
+				responseHeaders.setLocation(new URI(location));
+			} catch (URISyntaxException e) {
+				// return ApiResponseUtil.createResponseEntityFromException(e);
+			}
+
+			return new ResponseEntity<>(responseHeaders, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
 
 	@Override
 	public ResponseEntity deleteProcessScript(String indicatorId) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Received request to delete process scripts for indicatorId '{}'", indicatorId);
+		
+		String accept = request.getHeader("Accept");
+
+		/*
+		 * delete role with the specified id
+		 */
+			
+			boolean isDeleted;
+			try {
+				isDeleted = scriptManager.deleteScriptByIndicatorId(indicatorId);
+			
+			if(isDeleted)
+				return new ResponseEntity<>(HttpStatus.OK);
+			
+			} catch (ResourceNotFoundException e) {
+				return ApiUtils.createResponseEntityFromException(e);
+			}
+		
+		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
 	}
 
 	@Override
@@ -56,19 +110,75 @@ public class ScriptController extends BasePathController implements ProcessScrip
 
 	@Override
 	public ResponseEntity<List<ProcessScriptOverviewType>> getProcessScripts() {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Received request to get all process script metadata");
+		String accept = request.getHeader("Accept");
+
+		/*
+		 * retrieve all available roles
+		 * 
+		 * return them to client
+		 */
+
+		if (accept != null && accept.contains("application/json")){
+			
+			List<ProcessScriptOverviewType> roles = scriptManager.getAllScriptsMetadata();
+			
+			return new ResponseEntity<>(roles, HttpStatus.OK);
+			
+		} else{
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 	@Override
 	public ResponseEntity<List<ProcessScriptOverviewType>> getProcessScriptsForIndicator(String indicatorId) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Received request to get process script metadata for indicatorId '{}'", indicatorId);
+		String accept = request.getHeader("Accept");
+
+		/*
+		 * retrieve the role for the specified id
+		 */
+
+		if (accept != null && accept.contains("application/json")){
+			
+			List<ProcessScriptOverviewType> scripts = scriptManager.getScriptMetadataByIndicatorId(indicatorId);
+			
+			return new ResponseEntity<>(scripts, HttpStatus.OK);
+			
+		} else{
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 	@Override
 	public ResponseEntity updateProcessScriptAsBody(String indicatorId, ProcessScriptPUTInputType processScriptData) {
-		// TODO Auto-generated method stub
-		return null;
+		logger.info("Received request to update process script with indicatorId '{}'", indicatorId);
+
+		String accept = request.getHeader("Accept");
+
+		/*
+		 * analyse input data and save it within database
+		 */
+		try {
+			indicatorId = scriptManager.updateScriptForIndicatorId(processScriptData, indicatorId);
+		} catch (Exception e1) {
+			return ApiUtils.createResponseEntityFromException(e1);
+
+		}
+
+		if (indicatorId != null) {
+			HttpHeaders responseHeaders = new HttpHeaders();
+
+			String location = indicatorId;
+			try {
+				responseHeaders.setLocation(new URI(location));
+			} catch (URISyntaxException e) {
+				// return ApiResponseUtil.createResponseEntityFromException(e);
+			}
+
+			return new ResponseEntity<>(responseHeaders, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
--- src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/scripts/ScriptManager.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/scripts/ScriptManager.java
@@ -1,5 +1,7 @@
 package de.hsbo.kommonitor.datamanagement.api.impl.scripts;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
@@ -7,12 +9,43 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import de.hsbo.kommonitor.datamanagement.api.impl.exception.ResourceNotFoundException;
+import de.hsbo.kommonitor.datamanagement.model.scripts.ProcessScriptOverviewType;
+import de.hsbo.kommonitor.datamanagement.model.scripts.ProcessScriptPOSTInputType;
+import de.hsbo.kommonitor.datamanagement.model.scripts.ProcessScriptPUTInputType;
+
 @Transactional
 @Repository
 @Component
 public class ScriptManager {
 
 	private static Logger logger = LoggerFactory.getLogger(ScriptManager.class);
+
+	public String addScript(ProcessScriptPOSTInputType processScriptData) throws Exception{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean deleteScriptByIndicatorId(String indicatorId) throws ResourceNotFoundException{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public List<ProcessScriptOverviewType> getAllScriptsMetadata() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<ProcessScriptOverviewType> getScriptMetadataByIndicatorId(String indicatorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateScriptForIndicatorId(ProcessScriptPUTInputType processScriptData, String indicatorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 	
 
