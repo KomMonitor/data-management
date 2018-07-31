579ddf04b389c1b52e1565c52563631454023cad
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2018-07-31 12:38:32 +0200

    add initial setup of topics for kommonitor

---
+++ src/main/java/de/hsbo/kommonitor/datamanagement/config/InitialTopicsSetupConstants.java
@@ -0,0 +1,20 @@
+package de.hsbo.kommonitor.datamanagement.config;
+
+public class InitialTopicsSetupConstants {
+
+	public static final String SOCIAL_NAME = "Sozialstruktur";
+	public static final String SOCIAL_DESCRIPTION = "Das Thema Sozialstruktur umfasst Indikatoren zur Beschaffenheit einer Gesellschaft und ihrer Sozialstruktur";
+
+	public static final String DEMOGRAPHY_NAME = "Demographie";
+	public static final String DEMOGRAPHY_DESCRIPTION = "Das Thema Demographie umfasst Indikatoren zur Entwicklung einer Bevölkerung und ihrer Strukturen";
+
+	public static final String HABITATION_NAME = "Wohnen";
+	public static final String HABITATION_DESCRIPTION = "Das Thema Wohnen umfasst Indikatoren zur Beschaffenheit des Wohnungsmarkts und der Wohnqualität";
+	
+	public static final String MIGRATION_NAME = "Migration";
+	public static final String MIGRATION_DESCRIPTION = "Das Thema Migration umfasst Indikatoren zum Wanderungsverhalten von Ein- und Auswanderern innerhalb einer Gesellschaft";
+
+	public static final String ENVIRONMENT_NAME = "Umwelt";
+	public static final String ENVIRONMENT_DESCRIPTION = "Das Thema Umwelt umfasst Indikatoren und zur Beschaffenheit und zum (nachhaltigen) Umgang mit natürlichen Ressourcen sowie weiteren umwelt-politischen Bereichen";
+
+}
