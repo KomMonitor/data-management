a84a68be34379fb1b60cff776559a92a055630ff
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-01-22 10:34:56 +0100

    change basePath to "management"

--- src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/BasePathController.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/BasePathController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(BasePathController.BASE_PATH_KOMMONITOR_API_V1)
 public class BasePathController {
 
-	public static final String BASE_PATH_KOMMONITOR_API_V1 = "/rest/v1";
+	public static final String BASE_PATH_KOMMONITOR_API_V1 = "/management";
 
 }
