6fee1879397fb911075c6b6bc354089440511b4a
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2019-04-03 12:21:30 +0200

    prepare POM and app to switch between JAR and WAR build

--- src/main/java/de/hsbo/kommonitor/datamanagement/Runner.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/Runner.java
@@ -1,15 +1,15 @@
 package de.hsbo.kommonitor.datamanagement;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("de.hsbo.kommonitor")
 @EntityScan( basePackages = {"de.hsbo.kommonitor"} )
-public class Runner {
+public class Runner extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Runner.class, args);
