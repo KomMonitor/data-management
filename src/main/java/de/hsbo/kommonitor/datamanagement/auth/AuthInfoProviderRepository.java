5bc6086de487362dc8a8fe2d78047c0345a093c7
Sebastian Drost <s.drost@52north.org>
2020-06-23 19:37:27 +0200

    Adjust AuthInfoProvider retrieving

--- src/main/java/de/hsbo/kommonitor/datamanagement/auth/AuthInfoProviderRepository.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/auth/AuthInfoProviderRepository.java
@@ -1,10 +1,12 @@
 package de.hsbo.kommonitor.datamanagement.auth;
 
+import org.keycloak.KeycloakSecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.swing.text.html.Option;
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
@@ -26,11 +28,20 @@ public class AuthInfoProviderRepository {
      *
      * @return A {@link AuthInfoProvider} implementation
      */
-    public Optional<AuthInfoProvider> getAuthInfoProvider() {
+    public AuthInfoProvider getAuthInfoProvider() {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         Principal principal = attributes.getRequest().getUserPrincipal();
-        return authInfoProviderList.stream()
+        if (principal == null) {
+            throw new IllegalStateException("Cannot get AuthInfoProvider because there is no authenticated principal");
+        }
+        Optional<AuthInfoProvider> provider = authInfoProviderList.stream()
                 .filter(a -> a.supportsPrincipal(principal))
                 .findFirst();
+        if (!provider.isPresent()) {
+            throw new IllegalStateException(String.format("Cannot create an AuthInfoProvider because the " +
+                    "principal type %s is not supported.", principal.getClass()));
+        }
+        provider.get().setPrincipal(principal);
+        return provider.get();
     }
 }
