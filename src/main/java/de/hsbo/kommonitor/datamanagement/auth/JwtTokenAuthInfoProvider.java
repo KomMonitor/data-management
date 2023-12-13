2ebb44ea420b74c384f4a0447639a88c2127bff0
Sebastian Drost <s.drost@52north.org>
2023-12-13 15:51:46 +0100

    Add AuthenticationProvider for JwtTokens

---
+++ src/main/java/de/hsbo/kommonitor/datamanagement/auth/JwtTokenAuthInfoProvider.java
@@ -0,0 +1,27 @@
+package de.hsbo.kommonitor.datamanagement.auth;
+
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class JwtTokenAuthInfoProvider extends AuthInfoProvider<JwtAuthenticationToken> {
+
+    public JwtTokenAuthInfoProvider(JwtAuthenticationToken principal, String clientId, String adminRolePrefix, String publicRole) {
+        super(principal, clientId, adminRolePrefix, publicRole);
+    }
+
+    @Override
+    public Set<String> getOwnedRoles(JwtAuthenticationToken principal) {
+        List<String> roles = ((Map<String, List<String>>)principal.getTokenAttributes().get("realm_access")).get("roles");
+        return new HashSet<>(roles);
+    }
+
+    @Override
+    public boolean hasRealmAdminRole(JwtAuthenticationToken principal) {
+        return getOwnedRoles(getPrincipal()).stream().anyMatch(r -> r.equals(getAdminRolePrefix() + "-creator"));
+    }
+
+}
