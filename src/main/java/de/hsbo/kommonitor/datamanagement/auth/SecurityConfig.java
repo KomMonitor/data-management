0c067a950363ca0426554e8de371a85e5b7f5147
Sebastian Drost <s.drost@52north.org>
2023-12-14 11:10:00 +0100

    Migrate from deprecated EnableGlobalMethodSecurity to EnableMethodSecurity

--- src/main/java/de/hsbo/kommonitor/datamanagement/auth/SecurityConfig.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/auth/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,6 +23,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Value("${kommonitor.data-management.allowed-cors-origins}")
