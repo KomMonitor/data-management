fcfb1470c3fa4f4cf9cbb09a804ef445f7227190
Sebastian Drost <s.drost@52north.org>
2024-03-01 15:22:58 +0100

    Init role creation

--- src/main/java/de/hsbo/kommonitor/datamanagement/auth/KeycloakAdminService.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/auth/KeycloakAdminService.java
@@ -6,16 +6,35 @@ import de.hsbo.kommonitor.datamanagement.model.OrganizationalUnitInputType;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.GroupResource;
 import org.keycloak.admin.client.resource.GroupsResource;
+import org.keycloak.admin.client.resource.RolesResource;
 import org.keycloak.representations.idm.GroupRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class KeycloakAdminService {
 
     private static final String REALM_NAME = "kommonitor";
+
+    private static final String CLIENT_RESOURCES_ADMIN_ROLE_NAME = "client-resources-creator";
+    private static final String UNIT_RESOURCES_ADMIN_ROLE_NAME = "unit-resources-creator";
+    private static final String CLIENT_THEMES_ADMIN_ROLE_NAME = "client-themes-creator";
+    private static final String UNIT_THEMES_ADMIN_ROLE_NAME = "unit-themes-creator";
+    private static final String CLIENT_USERS_ADMIN_ROLE_NAME = "client-users-creator";
+    private static final String UNIT_USERS_ADMIN_ROLE_NAME = "unit-users-creator";
+
+    private static final List ADMIN_ROLES = Arrays.asList(
+            CLIENT_RESOURCES_ADMIN_ROLE_NAME,
+            UNIT_RESOURCES_ADMIN_ROLE_NAME,
+            CLIENT_THEMES_ADMIN_ROLE_NAME,
+            UNIT_THEMES_ADMIN_ROLE_NAME,
+            CLIENT_USERS_ADMIN_ROLE_NAME,
+            UNIT_USERS_ADMIN_ROLE_NAME
+            );
 
     @Autowired
     private Keycloak keycloak;
@@ -39,6 +58,12 @@ public class KeycloakAdminService {
         Response response = parentGroup.subGroup(group);
 
         return handleResponse(response);
+    }
+
+    public void createRolesForGroup(OrganizationalUnitInputType inputOrganizationalUnit) {
+        RolesResource rolesResource = keycloak.realm(REALM_NAME).roles();
+
+
     }
 
     public String handleResponse(Response response) throws KeycloakException {
