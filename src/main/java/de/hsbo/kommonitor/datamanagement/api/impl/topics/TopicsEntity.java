b9148a50b4be5e7b4984c8d6545eb9ba77cebc5e
Sebastian Drost <s.drost@52north.org>
2025-11-14 15:21:50 +0100

    Reformat code

--- src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/topics/TopicsEntity.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/api/impl/topics/TopicsEntity.java
@@ -6,133 +6,134 @@ import de.hsbo.kommonitor.datamanagement.model.TopicTypeEnum;
 import org.hibernate.annotations.GenericGenerator;
 
 import jakarta.persistence.*;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity(name = "Topics")
 public class TopicsEntity {
-	
-	  @Id
-	  @GeneratedValue(generator = "UUID")
-	  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-	  private String topicId = null;
 
-	  private String topicName = null;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String topicId = null;
 
-	  @Column(columnDefinition="text")
-	  private String topicDescription = null;
-	  
-	  private TopicTypeEnum topicType = null;
-	  private TopicResourceEnum topicResource = null;
-	  private int displayOrder = 0;
-	  
-	  @ElementCollection()
+    private String topicName = null;
+
+    @Column(columnDefinition = "text")
+    private String topicDescription = null;
+
+    private TopicTypeEnum topicType = null;
+    private TopicResourceEnum topicResource = null;
+    private int displayOrder = 0;
+
+    @ElementCollection()
 //	  @OnDelete(action= OnDeleteAction.CASCADE)
 //	  @JoinColumn(name = "main_topic_id", referencedColumnName = "topicId")
-	  @CollectionTable(name = "topics_subtopics", joinColumns = @JoinColumn(name = "main_topic_id", referencedColumnName = "topicId"))
-	  @Column(name = "sub_topic")
-	  private Collection<TopicsEntity> subTopics;
+    @CollectionTable(name = "topics_subtopics", joinColumns = @JoinColumn(name = "main_topic_id", referencedColumnName = "topicId"))
+    @Column(name = "sub_topic")
+    private Collection<TopicsEntity> subTopics;
 
-	@ManyToMany(mappedBy = "topicFavourites")
-	private Set<UserInfoEntity> userFavorites = new HashSet<>();
+    @ManyToMany(mappedBy = "topicFavourites")
+    private Set<UserInfoEntity> userFavorites = new HashSet<>();
 
-	public Set<UserInfoEntity> getUserFavorites() {
-		return userFavorites;
-	}
+    public Set<UserInfoEntity> getUserFavorites() {
+        return userFavorites;
+    }
 
-	public void setUserFavorites(Set<UserInfoEntity> userFavorites) {
-		this.userFavorites = userFavorites;
-	}
-	  
-	  /*
-	   * default constructor is required by hibernate / jpa
-	   */
-	  
-	  public TopicsEntity(){
-		  
-	  }
-	  
+    public void setUserFavorites(Set<UserInfoEntity> userFavorites) {
+        this.userFavorites = userFavorites;
+    }
 
-	  public String getTopicId() {
-	    return topicId;
-	  }
+    /*
+     * default constructor is required by hibernate / jpa
+     */
 
-	  public TopicsEntity topicName(String topicName) {
-	    this.topicName = topicName;
-	    return this;
-	  }
+    public TopicsEntity() {
 
-	  public String getTopicName() {
-	    return topicName;
-	  }
-
-	  public void setTopicName(String topicName) {
-	    this.topicName = topicName;
-	  }
-
-	  public TopicsEntity topicDescription(String topicDescription) {
-	    this.topicDescription = topicDescription;
-	    return this;
-	  }
-
-	  public String getTopicDescription() {
-	    return topicDescription;
-	  }
-
-	  public void setTopicDescription(String topicDescription) {
-	    this.topicDescription = topicDescription;
-	  }
+    }
 
 
-	public TopicTypeEnum getTopicType() {
-		return topicType;
-	}
+    public String getTopicId() {
+        return topicId;
+    }
+
+    public TopicsEntity topicName(String topicName) {
+        this.topicName = topicName;
+        return this;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public TopicsEntity topicDescription(String topicDescription) {
+        this.topicDescription = topicDescription;
+        return this;
+    }
+
+    public String getTopicDescription() {
+        return topicDescription;
+    }
+
+    public void setTopicDescription(String topicDescription) {
+        this.topicDescription = topicDescription;
+    }
 
 
-	public void setTopicType(TopicTypeEnum topicType) {
-		this.topicType = topicType;
-	}
-
-	public int getDisplayOrder() {
-		return displayOrder;
-	}
-
-	public void setDisplayOrder(int displayOrder) {
-		this.displayOrder = displayOrder;
-	}
-
-	public TopicResourceEnum getTopicResource() {
-		return topicResource;
-	}
+    public TopicTypeEnum getTopicType() {
+        return topicType;
+    }
 
 
-	public void setTopicResource(TopicResourceEnum topicResource) {
-		this.topicResource = topicResource;
-	}
+    public void setTopicType(TopicTypeEnum topicType) {
+        this.topicType = topicType;
+    }
+
+    public int getDisplayOrder() {
+        return displayOrder;
+    }
+
+    public void setDisplayOrder(int displayOrder) {
+        this.displayOrder = displayOrder;
+    }
+
+    public TopicResourceEnum getTopicResource() {
+        return topicResource;
+    }
 
 
-	public Collection<TopicsEntity> getSubTopics() {
-		return subTopics;
-	}
+    public void setTopicResource(TopicResourceEnum topicResource) {
+        this.topicResource = topicResource;
+    }
 
 
-	public void setSubTopics(Collection<TopicsEntity> subTopics) {
-		this.subTopics = subTopics;
-	}
-	
-	public void addSubTopicsIfNotExists(Collection<TopicsEntity> subTopics) {
-		for (TopicsEntity topicsEntity : subTopics) {
-			if(! this.subTopics.contains(topicsEntity)){
-				this.subTopics.add(topicsEntity);
-			}
-		}
-	}
-	
-	public void addSubTopicIfNotExists(TopicsEntity subTopic) {
-		if(! this.subTopics.contains(subTopic)){
-			this.subTopics.add(subTopic);
-		}
-	}
+    public Collection<TopicsEntity> getSubTopics() {
+        return subTopics;
+    }
+
+
+    public void setSubTopics(Collection<TopicsEntity> subTopics) {
+        this.subTopics = subTopics;
+    }
+
+    public void addSubTopicsIfNotExists(Collection<TopicsEntity> subTopics) {
+        for (TopicsEntity topicsEntity : subTopics) {
+            if (!this.subTopics.contains(topicsEntity)) {
+                this.subTopics.add(topicsEntity);
+            }
+        }
+    }
+
+    public void addSubTopicIfNotExists(TopicsEntity subTopic) {
+        if (!this.subTopics.contains(subTopic)) {
+            this.subTopics.add(subTopic);
+        }
+    }
 
 }
