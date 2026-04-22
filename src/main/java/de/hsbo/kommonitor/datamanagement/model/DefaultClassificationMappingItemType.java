e8b6052a9c7e6170ed12df399928c2f12f0e3903
Sebastian Drost <s.drost@52north.org>
2026-04-22 11:30:33 +0200

    Reformat code

--- src/main/java/de/hsbo/kommonitor/datamanagement/model/DefaultClassificationMappingItemType.java
+++ src/main/java/de/hsbo/kommonitor/datamanagement/model/DefaultClassificationMappingItemType.java
@@ -23,103 +23,105 @@ import jakarta.validation.constraints.NotNull;
 @jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2024-04-09T13:07:45.192171293Z[GMT]")
 
 @Entity(name = "DefaultClassificationMappingItemType")
-public class DefaultClassificationMappingItemType   {
-	
-  @Id
-  @GeneratedValue(generator = "UUID")
-  @UuidGenerator
-  private String mappingId; 
-	
-  @JsonProperty("spatialUnitId")
-  private String spatialUnitId = null;
+public class DefaultClassificationMappingItemType {
 
-  @JsonProperty("breaks")
-  @Valid
-  private List<Float> breaks = new ArrayList<Float>();
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @UuidGenerator
+    private String mappingId;
 
-  public DefaultClassificationMappingItemType spatialUnitId(String spatialUnitId) {
-    this.spatialUnitId = spatialUnitId;
-    return this;
-  }
+    @JsonProperty("spatialUnitId")
+    private String spatialUnitId = null;
 
-  /**
-   * spatial unit id for manual classification
-   * @return spatialUnit
-   **/
-  @Schema(required = true, description = "spatial unit id for manual classification")
-      @NotNull
+    @JsonProperty("breaks")
+    @Valid
+    private List<Float> breaks = new ArrayList<>();
+
+    public DefaultClassificationMappingItemType spatialUnitId(String spatialUnitId) {
+        this.spatialUnitId = spatialUnitId;
+        return this;
+    }
+
+    /**
+     * spatial unit id for manual classification
+     *
+     * @return spatialUnit
+     **/
+    @Schema(required = true, description = "spatial unit id for manual classification")
+    @NotNull
 
     public String getSpatialUnitId() {
-    return spatialUnitId;
-  }
+        return spatialUnitId;
+    }
 
-  public void setSpatialUnitId(String spatialUnitId) {
-    this.spatialUnitId = spatialUnitId;
-  }
+    public void setSpatialUnitId(String spatialUnitId) {
+        this.spatialUnitId = spatialUnitId;
+    }
 
-  public DefaultClassificationMappingItemType breaks(List<Float> breaks) {
-    this.breaks = breaks;
-    return this;
-  }
+    public DefaultClassificationMappingItemType breaks(List<Float> breaks) {
+        this.breaks = breaks;
+        return this;
+    }
 
-  public DefaultClassificationMappingItemType addBreaksItem(Float breaksItem) {
-    this.breaks.add(breaksItem);
-    return this;
-  }
+    public DefaultClassificationMappingItemType addBreaksItem(Float breaksItem) {
+        this.breaks.add(breaksItem);
+        return this;
+    }
 
-  /**
-   * array of numeric break values
-   * @return breaks
-   **/
-  @Schema(required = true, description = "array of numeric break values")
-      @NotNull
+    /**
+     * array of numeric break values
+     *
+     * @return breaks
+     **/
+    @Schema(required = true, description = "array of numeric break values")
+    @NotNull
 
     public List<Float> getBreaks() {
-    return breaks;
-  }
-
-  public void setBreaks(List<Float> breaks) {
-    this.breaks = breaks;
-  }
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
+        return breaks;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    public void setBreaks(List<Float> breaks) {
+        this.breaks = breaks;
     }
-    DefaultClassificationMappingItemType defaultClassificationMappingItemType = (DefaultClassificationMappingItemType) o;
-    return Objects.equals(this.spatialUnitId, defaultClassificationMappingItemType.spatialUnitId) &&
-        Objects.equals(this.breaks, defaultClassificationMappingItemType.breaks);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(spatialUnitId, breaks);
-  }
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class DefaultClassificationMappingItemType {\n");
-    
-    sb.append("    spatialUnitId: ").append(toIndentedString(spatialUnitId)).append("\n");
-    sb.append("    breaks: ").append(toIndentedString(breaks)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        DefaultClassificationMappingItemType defaultClassificationMappingItemType = (DefaultClassificationMappingItemType) o;
+        return Objects.equals(this.spatialUnitId, defaultClassificationMappingItemType.spatialUnitId) &&
+                Objects.equals(this.breaks, defaultClassificationMappingItemType.breaks);
     }
-    return o.toString().replace("\n", "\n    ");
-  }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(spatialUnitId, breaks);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class DefaultClassificationMappingItemType {\n");
+
+        sb.append("    spatialUnitId: ").append(toIndentedString(spatialUnitId)).append("\n");
+        sb.append("    breaks: ").append(toIndentedString(breaks)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
 }
